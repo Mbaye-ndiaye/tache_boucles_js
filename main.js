36ec76c833b41cf632a439af10d56e17ecb06c0a
Mbaye Ndiaye <babacarndiay546@gmail.com>
2023-08-15 23:30:21 +0000

    tache boucles

---
+++ main.js
@@ -0,0 +1,18 @@
+// La definition des boucles 
+
+let boucles = prompt('Saisir une boucle');
+
+if('while') {
+    alert(`nous permettre de répéter une série d’instructions
+     tant qu’une condition donnée est vraie `);
+} else if ('For'); {
+   (`est une structure de contrôle de programmation qui permet de 
+   répéter l'exécution d\'une séquence d\'instructions.`);
+
+} elseif('do while'); {
+    alert(` qui nous permet d'exécuter une instruction jusqu'à 
+    ce qu'une condition de test ne soit plus vérifiée`);
+}; 
+
+
+
